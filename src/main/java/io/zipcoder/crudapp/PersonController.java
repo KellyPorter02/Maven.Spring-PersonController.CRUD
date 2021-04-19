76fca36becc7bfd41e6fde66c73cba56482db8e5
KellyPorter02 <porter.kelly02@gmail.com>
2021-04-18 23:03:34 -0400

    exercise compelete

--- src/main/java/io/zipcoder/crudapp/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -17,26 +17,32 @@ public class PersonController {
         this.service = service;
     }
 
+    // http://localhost:8080/people
     @GetMapping("/people")
     public ResponseEntity<Iterable<Person>> getPersonList() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
+    // http://localhost:8080/people?
     @GetMapping("/people/{id}")
     public ResponseEntity<Person> getPerson(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
+    // http://localhost:8080/people?firstName=Kelly&lastName=Porter  ** will auto provide id
+    // http://localhost:8080/people?id=1&firstName=Kelly&lastName=Porter ** will only
     @PostMapping("/people")
-    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+    public ResponseEntity<Person> createPerson(Person person) {
         return new ResponseEntity<>(service.create(person), HttpStatus.CREATED);
     }
 
+    // http://localhost:8080/people?
     @PutMapping("/people/{id}")
     public ResponseEntity<Person> updatePerson(@PathVariable Long id, Person person) {
         return new ResponseEntity<>(service.update(id, person), HttpStatus.OK);
     }
 
+    // http://localhost:8090/people/1?
     @DeleteMapping("/people/{id}")
     public ResponseEntity<Boolean> deletePerson(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
